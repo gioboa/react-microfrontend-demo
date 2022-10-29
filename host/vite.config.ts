da9a7728102c6b86186d536cabd636e5ea6f10b5
gioboa <giorgiob.boa@gmail.com>
2022-10-29 20:39:56 +0200

    fix config for dev mode

--- host/vite.config.ts
+++ host/vite.config.ts
@@ -2,6 +2,7 @@ import { federation } from '@gioboa/vite-module-federation';
 import { createEsBuildAdapter } from '@softarc/native-federation-esbuild';
 import react from '@vitejs/plugin-react';
 import { defineConfig } from 'vite';
+import { importMaps } from './module-federation/vite-importmap-shim';
 
 export default defineConfig(async ({ command }) => ({
 	server: {
@@ -10,6 +11,11 @@ export default defineConfig(async ({ command }) => ({
 		},
 	},
 	plugins: [
+		command === 'serve' &&
+			importMaps({
+				'__x00__react/jsx-dev-runtime':
+					'https://esm.sh/react@18.0.0?pin=v74&path=/jsx-dev-runtime',
+			}),
 		await federation({
 			options: {
 				workspaceRoot: __dirname,
