93674a985a1e455cf413673c63132e09e7a58ea3
gioboa <giorgiob.boa@gmail.com>
2026-02-16 22:31:00 +0100

    feat: bump deps 🍍

--- host/vite.config.ts
+++ host/vite.config.ts
@@ -10,6 +10,7 @@ export default defineConfig(() => ({
 	},
 	plugins: [
 		federation({
+			dts: false,
 			name: 'host',
 			remotes: {
 				remote: {
--- remote/vite.config.ts
+++ remote/vite.config.ts
@@ -24,6 +24,7 @@ export default defineConfig(({ mode }) => {
 				},
 			},
 			federation({
+				dts: false,
 				filename: 'remoteEntry.js',
 				name: 'remote',
 				exposes: {
